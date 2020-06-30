ee49090bc4f614e846fb5d78856bdd690f526efc
Duke09 <debopriyo09@outlook.com>
2020-06-30 14:19:08 +0530

    installed social django and added list of ALLOWED HOSTS

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z&c*m+^%)&hk72e+d8#kj2f!qmu1#^yeu_k4sk49@#km6*g$8p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
 ]
 
 MIDDLEWARE = [
