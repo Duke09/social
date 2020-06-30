cf0e898fead7bb13231d3bc1cc3def51bd3f3f88
Duke09 <debopriyo09@outlook.com>
2020-06-30 10:11:42 +0530

    Password reset config and templates

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,7 +5,52 @@ from .views import *
 
 urlpatterns = [
     # path('login/', user_login, name='login'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', dashboard, name='dashboard'),
+    path(
+        '', 
+        dashboard, 
+        name='dashboard'
+    ),
+    # Authentication
+    path(
+        'login/', 
+        auth_views.LoginView.as_view(), 
+        name='login'
+    ),
+    path(
+        'logout/', 
+        auth_views.LogoutView.as_view(), 
+        name='logout'
+    ),
+    # Password Change
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change'
+    ),
+    path(
+        'password_change/done/',
+        auth_views. PasswordChangeDoneView.as_view(),
+        name='password_change_done'
+    ),
+    # Password reset
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    path(
+        'password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/,uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
 ]
